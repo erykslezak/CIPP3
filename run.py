4608c12361beff6c97bb12af61f449565c2926d1
erykslezak <erykslezak@outlook.ie>
2021-12-14 00:10:59 +0000

    Add grid list variable and function print_grid that prints out the list to run.py

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Variable 'grid' that is a list of empty strings to create grid.
+grid = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+# Print grid
+def print_grid():
+    """
+    Prints the list from the grid variable. Sets position for each item
+    within the list.
+    """
+    print(" Game Grid ")
+    print(" " + grid[1] + " | " + grid[2] + " | " + grid[3] + "  ")
+    print("---|---|---")
+    print(" " + grid[4] + " | " + grid[5] + " | " + grid[6] + "  ")
+    print("---|---|---")
+    print(" " + grid[7] + " | " + grid[8] + " | " + grid[9] + "  ")
+    print("\n")
+
+
+print_grid()
